5ff90048252a441d246094c430b8feaa218780bd
YamiOdymel <yamiodymel@gmail.com>
2019-11-10 19:28:01 +0800

    更新 SubQuery

--- subquery.go
+++ subquery.go
@@ -2,9 +2,7 @@ package rushia
 
 // SubQuery 是單個子指令，任何的變更都會回傳一份複製子指令來避免多個 Goroutine 編輯同個子指令指標建構體。
 type SubQuery struct {
-	builder Builder
-	// PageLimit 限制了一頁僅能有幾筆資料。
-	PageLimit int
+	query Query
 }
 
 //=======================================================
@@ -13,7 +11,7 @@ type SubQuery struct {
 
 // Table 能夠指定資料表格的名稱。
 func (s SubQuery) Table(tableName ...string) SubQuery {
-	s.builder = s.builder.Table(tableName...)
+	s.query = s.query.Table(tableName...)
 	return s
 }
 
@@ -23,7 +21,7 @@ func (s SubQuery) Table(tableName ...string) SubQuery {
 
 // Get 會取得多列的資料結果，傳入的參數為欲取得的欄位名稱，不傳入參數表示取得所有欄位。
 func (s SubQuery) Get(columns ...string) SubQuery {
-	s.builder.query, s.builder.params = s.builder.Get(columns...)
+	s.query.query, s.query.params = s.query.Get(columns...)
 	return s
 }
 
@@ -33,19 +31,19 @@ func (s SubQuery) Get(columns ...string) SubQuery {
 
 // Limit 能夠在 SQL 查詢指令中建立限制筆數的條件。
 func (s SubQuery) Limit(from int, count ...int) SubQuery {
-	s.builder = s.builder.Limit(from, count...)
+	s.query = s.query.Limit(from, count...)
 	return s
 }
 
 // OrderBy 會依照指定的欄位來替結果做出排序（例如：`DESC`、`ASC`）。
 func (s SubQuery) OrderBy(column string, args ...interface{}) SubQuery {
-	s.builder = s.builder.OrderBy(column, args...)
+	s.query = s.query.OrderBy(column, args...)
 	return s
 }
 
 // GroupBy 會在執行 SQL 指令時依照特定的欄位來做執行區分。
 func (s SubQuery) GroupBy(columns ...string) SubQuery {
-	s.builder = s.builder.GroupBy(columns...)
+	s.query = s.query.GroupBy(columns...)
 	return s
 }
 
@@ -56,7 +54,7 @@ func (s SubQuery) GroupBy(columns ...string) SubQuery {
 // RawQuery 會接收傳入的變數來執行傳入的 SQL 執行語句，變數可以在語句中以 `?`（Prepared Statements）使用來避免 SQL 注入攻擊。
 // 這會將多筆資料映射到本地的建構體切片、陣列上。
 func (s SubQuery) RawQuery(query string, values ...interface{}) SubQuery {
-	s.builder.query, s.builder.params = s.builder.RawQuery(query, values...)
+	s.query.query, s.query.params = s.query.RawQuery(query, values...)
 	return s
 }
 
@@ -66,25 +64,25 @@ func (s SubQuery) RawQuery(query string, values ...interface{}) SubQuery {
 
 // Where 會增加一個 `WHERE AND` 條件式。
 func (s SubQuery) Where(args ...interface{}) SubQuery {
-	s.builder = s.builder.Where(args...)
+	s.query = s.query.Where(args...)
 	return s
 }
 
 // OrWhere 會增加一個 `WHERE OR` 條件式。
 func (s SubQuery) OrWhere(args ...interface{}) SubQuery {
-	s.builder = s.builder.OrWhere(args...)
+	s.query = s.query.OrWhere(args...)
 	return s
 }
 
 // Having 會增加一個 `HAVING AND` 條件式。
 func (s SubQuery) Having(args ...interface{}) SubQuery {
-	s.builder = s.builder.Having(args...)
+	s.query = s.query.Having(args...)
 	return s
 }
 
 // OrHaving 會增加一個 `HAVING OR` 條件式。
 func (s SubQuery) OrHaving(args ...interface{}) SubQuery {
-	s.builder = s.builder.OrHaving(args...)
+	s.query = s.query.OrHaving(args...)
 	return s
 }
 
@@ -94,36 +92,36 @@ func (s SubQuery) OrHaving(args ...interface{}) SubQuery {
 
 // LeftJoin 會向左插入一個資料表格。
 func (s SubQuery) LeftJoin(table interface{}, condition string) SubQuery {
-	s.builder = s.builder.LeftJoin(table, condition)
+	s.query = s.query.LeftJoin(table, condition)
 	return s
 }
 
 // RightJoin 會向右插入一個資料表格。
 func (s SubQuery) RightJoin(table interface{}, condition string) SubQuery {
-	s.builder = s.builder.RightJoin(table, condition)
+	s.query = s.query.RightJoin(table, condition)
 	return s
 }
 
 // InnerJoin 會內部插入一個資料表格。
 func (s SubQuery) InnerJoin(table interface{}, condition string) SubQuery {
-	s.builder = s.builder.InnerJoin(table, condition)
+	s.query = s.query.InnerJoin(table, condition)
 	return s
 }
 
 // NaturalJoin 會自然插入一個資料表格。
 func (s SubQuery) NaturalJoin(table interface{}, condition string) SubQuery {
-	s.builder = s.builder.NaturalJoin(table, condition)
+	s.query = s.query.NaturalJoin(table, condition)
 	return s
 }
 
 // JoinWhere 能夠建立一個基於 `WHERE AND` 的條件式給某個指定的插入資料表格。
 func (s SubQuery) JoinWhere(table interface{}, args ...interface{}) SubQuery {
-	s.builder = s.builder.JoinWhere(table, args...)
+	s.query = s.query.JoinWhere(table, args...)
 	return s
 }
 
 // JoinOrWhere 能夠建立一個基於 `WHERE OR` 的條件式給某個指定的插入資料表格。
 func (s SubQuery) JoinOrWhere(table interface{}, args ...interface{}) SubQuery {
-	s.builder = s.builder.JoinOrWhere(table, args...)
+	s.query = s.query.JoinOrWhere(table, args...)
 	return s
 }
--- subquery_test.go
+++ subquery_test.go
@@ -8,75 +8,71 @@ import (
 
 var subQuery SubQuery
 
-func TestSubQueryMain(t *testing.T) {
-	builder = New()
-}
-
 func TestSubQueryGetx(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").Get()
-	assertEqual(assert, "SELECT * FROM Users", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Get()
+	assertEqual(assert, "SELECT * FROM Users", subQuery.query.query)
 
-	subQuery = builder.SubQuery().Table("Users").Get("Username", "Password")
-	assertEqual(assert, "SELECT Username, Password FROM Users", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Get("Username", "Password")
+	assertEqual(assert, "SELECT Username, Password FROM Users", subQuery.query.query)
 }
 
 func TestSubQueryWhere(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").Where("ID", 1).Where("Username", "admin").Get()
-	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? AND Username = ?", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Where("ID", 1).Where("Username", "admin").Get()
+	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? AND Username = ?", subQuery.query.query)
 }
 
 func TestSubQueryOrWhere(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").Where("FirstName", "John").OrWhere("FirstName", "Peter").Get()
-	assertEqual(assert, "SELECT * FROM Users WHERE FirstName = ? OR FirstName = ?", subQuery.builder.query)
-	subQuery = builder.SubQuery().Table("Users").Where("A = B").OrWhere("(A = C OR A = D)").Get()
-	assertEqual(assert, "SELECT * FROM Users WHERE A = B OR (A = C OR A = D)", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Where("FirstName", "John").OrWhere("FirstName", "Peter").Get()
+	assertEqual(assert, "SELECT * FROM Users WHERE FirstName = ? OR FirstName = ?", subQuery.query.query)
+	subQuery = NewSubQuery().Table("Users").Where("A = B").OrWhere("(A = C OR A = D)").Get()
+	assertEqual(assert, "SELECT * FROM Users WHERE A = B OR (A = C OR A = D)", subQuery.query.query)
 }
 
 func TestSubQueryWhereHaving(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").Where("ID", 1).Having("Username", "admin").Get()
-	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? HAVING Username = ?", subQuery.builder.query)
-	subQuery = builder.SubQuery().Table("Users").Where("ID", 1).Having("Username", "admin").OrHaving("Password", "test").Get()
-	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? HAVING Username = ? OR Password = ?", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Where("ID", 1).Having("Username", "admin").Get()
+	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? HAVING Username = ?", subQuery.query.query)
+	subQuery = NewSubQuery().Table("Users").Where("ID", 1).Having("Username", "admin").OrHaving("Password", "test").Get()
+	assertEqual(assert, "SELECT * FROM Users WHERE ID = ? HAVING Username = ? OR Password = ?", subQuery.query.query)
 }
 
 func TestSubQueryLimit(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").Limit(10).Get()
-	assertEqual(assert, "SELECT * FROM Users LIMIT 10", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").Limit(10).Get()
+	assertEqual(assert, "SELECT * FROM Users LIMIT 10", subQuery.query.query)
 }
 
 func TestSubQueryOrderBy(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").OrderBy("ID", "ASC").OrderBy("Login", "DESC").OrderBy("RAND()").Get()
-	assertEqual(assert, "SELECT * FROM Users ORDER BY ID ASC, Login DESC, RAND()", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").OrderBy("ID", "ASC").OrderBy("Login", "DESC").OrderBy("RAND()").Get()
+	assertEqual(assert, "SELECT * FROM Users ORDER BY ID ASC, Login DESC, RAND()", subQuery.query.query)
 }
 
 func TestSubQueryOrderByField(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").OrderBy("UserGroup", "ASC", "SuperUser", "Admin", "Users").Get()
-	assertEqual(assert, "SELECT * FROM Users ORDER BY FIELD (UserGroup, ?, ?, ?) ASC", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").OrderBy("UserGroup", "ASC", "SuperUser", "Admin", "Users").Get()
+	assertEqual(assert, "SELECT * FROM Users ORDER BY FIELD (UserGroup, ?, ?, ?) ASC", subQuery.query.query)
 }
 
 func TestSubQueryGroupBy(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().Table("Users").GroupBy("Name").Get()
-	assertEqual(assert, "SELECT * FROM Users GROUP BY Name", subQuery.builder.query)
-	subQuery = builder.SubQuery().Table("Users").GroupBy("Name", "ID").Get()
-	assertEqual(assert, "SELECT * FROM Users GROUP BY Name, ID", subQuery.builder.query)
+	subQuery = NewSubQuery().Table("Users").GroupBy("Name").Get()
+	assertEqual(assert, "SELECT * FROM Users GROUP BY Name", subQuery.query.query)
+	subQuery = NewSubQuery().Table("Users").GroupBy("Name", "ID").Get()
+	assertEqual(assert, "SELECT * FROM Users GROUP BY Name, ID", subQuery.query.query)
 }
 
 func TestSubQueryJoinx(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		LeftJoin("Users", "Products.TenantID = Users.TenantID").
 		Where("Users.ID", 6).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.query.query)
 
 	query, _ := builder.
 		Table("Products").
@@ -85,56 +81,56 @@ func TestSubQueryJoinx(t *testing.T) {
 		Get("Users.Name", "Products.ProductName")
 	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", query)
 
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		RightJoin("Users", "Products.TenantID = Users.TenantID").
 		Where("Users.ID", 6).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products RIGHT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products RIGHT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.query.query)
 
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		InnerJoin("Users", "Products.TenantID = Users.TenantID").
 		Where("Users.ID", 6).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products INNER JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products INNER JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.query.query)
 
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		NaturalJoin("Users", "Products.TenantID = Users.TenantID").
 		Where("Users.ID", 6).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products NATURAL JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products NATURAL JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.query.query)
 
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		LeftJoin("Users", "Products.TenantID = Users.TenantID").
 		RightJoin("Posts", "Products.TenantID = Posts.TenantID").
 		Where("Users.ID", 6).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products RIGHT JOIN Posts ON (Products.TenantID = Posts.TenantID) LEFT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products RIGHT JOIN Posts ON (Products.TenantID = Posts.TenantID) LEFT JOIN Users ON (Products.TenantID = Users.TenantID) WHERE Users.ID = ?", subQuery.query.query)
 }
 
 func TestSubQueryJoinWhere(t *testing.T) {
 	assert := assert.New(t)
-	subQuery = builder.SubQuery().
+	subQuery = NewSubQuery().
 		Table("Products").
 		LeftJoin("Users", "Products.TenantID = Users.TenantID").
 		JoinOrWhere("Users", "Users.TenantID", 5).
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID OR Users.TenantID = ?)", subQuery.builder.query)
-	subQuery = builder.SubQuery().
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID OR Users.TenantID = ?)", subQuery.query.query)
+	subQuery = NewSubQuery().
 		Table("Products").
 		LeftJoin("Users", "Products.TenantID = Users.TenantID").
 		JoinWhere("Users", "Users.Username", "Wow").
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID AND Users.Username = ?)", subQuery.builder.query)
-	subQuery = builder.SubQuery().
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID AND Users.Username = ?)", subQuery.query.query)
+	subQuery = NewSubQuery().
 		Table("Products").
 		LeftJoin("Users", "Products.TenantID = Users.TenantID").
 		RightJoin("Posts", "Products.TenantID = Posts.TenantID").
 		JoinWhere("Posts", "Posts.Username", "Wow").
 		JoinWhere("Users", "Users.Username", "Wow").
 		Get("Users.Name", "Products.ProductName")
-	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID AND Users.Username = ?) RIGHT JOIN Posts ON (Products.TenantID = Posts.TenantID AND Posts.Username = ?)", subQuery.builder.query)
+	assertEqual(assert, "SELECT Users.Name, Products.ProductName FROM Products LEFT JOIN Users ON (Products.TenantID = Users.TenantID AND Users.Username = ?) RIGHT JOIN Posts ON (Products.TenantID = Posts.TenantID AND Posts.Username = ?)", subQuery.query.query)
 }
