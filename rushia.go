b4b800ba043c5d65d89d12895e53742d18910ca8
YamiOdymel <yamiodymel@gmail.com>
2019-11-10 19:29:03 +0800

    重新命名 New 函式

--- rushia.go
+++ rushia.go
@@ -1,10 +1,28 @@
 package rushia
 
-// New 會建立一個新的 SQL 建置工具。
-func New() Builder {
-	return newBuilder()
+// NewQuery 會建立一個新的 SQL 建置工具。
+func NewQuery() Query {
+	return Query{}
 }
 
+// NewSubQuery 會建立一個新的子指令（Sub Query），這讓你可以將子指令傳入其他的條件式（例如：`WHERE`），
+// 若欲將子指令傳入插入（Join）條件中，必須在參數指定此子指令的別名。
+func NewSubQuery(alias ...string) SubQuery {
+	subQuery := SubQuery{
+		query: NewQuery(),
+	}
+	if len(alias) > 0 {
+		subQuery.query.alias = alias[0]
+	}
+	return subQuery
+}
+
+// NewTimestamp 會建立一個新的 SQL 時間戳記輔助工具。
+func NewTimestamp() Timestamp {
+	return Timestamp{}
+}
+
+// NewMigration 會建立表格 SQL 輔助工具。
 func NewMigration() Migration {
-	return newMigration()
+	return Migration{}
 }
